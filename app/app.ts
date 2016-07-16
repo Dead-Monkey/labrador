7fdbeca0b813f77b87628edf1b47a5ed71a06319
edish edish <edish edish>
2016-07-16 17:02:48 +0300

    Orientation landscape, fullscreen, start axis

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,10 @@
-import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
-import {StatusBar} from 'ionic-native';
-import {StartComponent} from './+start/start.component';
+import { Component } from '@angular/core';
+import { Platform, ionicBootstrap } from 'ionic-angular';
+import { Insomnia } from 'ionic-native';
+import { StartComponent } from './+start/start.component';
+
+//4dev
+import { GameComponent } from './+game/game.component';
 
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
@@ -11,12 +14,18 @@ export class MyApp {
   private rootPage: any;
 
   constructor(private platform: Platform) {
-    this.rootPage = StartComponent;
+    // this.rootPage = StartComponent;
+    // 4dev
+    this.rootPage = GameComponent;
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
+      Insomnia.keepAwake()
+        .then(
+        () => console.log('Insomnia success'),
+        () => console.log('Insomnia error')
+        );
     });
   }
 }
